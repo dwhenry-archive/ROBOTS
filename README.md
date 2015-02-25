c35b65a76e99e2ba8e6fc3f45e8b62ce3fa1e835
David Henry <dw_henry@yahoo.com.au>
2015-02-24 20:41:58 -0800

    render the image to html img tag

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ROBOTS
+
+## Installation
+
+    brew update # required to get latest version of libpng
+
+    brew install homebrew/science/opencv
+
+    npm install opencv
+
+# Running the application
+
+    node streamtest.js
+
+# Adding packages
+
+When you add a package it is important that you save it to the packaging tool.
+
+This can easily be done by adding a ```-s``` option to the npm install command.
+
+i.e.
+
+    npm install --save opencv-node
--- old node/streamtest.js
+++ old node/streamtest.js
@@ -1,31 +1,63 @@
 var cv = require('opencv');
+var fs = require('fs');
+var util = require('util');
+// var Buffer = require('buffer');
 
-try {
-  var vid = new cv.VideoCapture(0);
-  var window = new cv.NamedWindow('Video', 0)
-  
-  setInterval(function() {
-    vid.read(function(err, im) {
-      if (err) throw err;
-      console.log(im.size())
-      if (im.size()[0] > 0 && im.size()[1] > 0){
-        window.show(im);
-      }
-      window.blockingWaitKey(0, 50);
+// var writeStream = fs.createWriteStream('');
+var vid = new cv.VideoCapture(0);
+
+
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    // process HTTP request. Since we're writing just WebSockets server
+    // we don't have to implement anything.
+});
+server.listen(1337, function() { });
+
+// create the server
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+var clients = [];
+// WebSocket server
+wsServer.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+    var index = clients.push(connection) - 1;
+
+    // This is the most important callback for us, we'll handle
+    // all messages from users here.
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            // process WebSocket message
+        }
     });
-  }, 20);
 
-  vid.read(function(err, im){
-    if (err) throw err;
-    if (im.size()[0] > 0 && im.size()[1] > 0){
+    setTimeout(sendImage, 10);
 
-      im.save('./pic.jpg')
+    function sendImage() {
+      vid.read(function(err, mat){
+        if (err) return self.emit('error', err);
 
-      console.log('Image saved to ./pic.jpg');
-    } else {
-      console.log("Camera didn't return image")
+        var img = mat.toBuffer()
+        var data = img.toString('base64')
+
+        var json = JSON.stringify({ type:'image', data: data });
+        for (var i=0; i < clients.length; i++) {
+          clients[i].sendUTF(json);
+        }
+      })
+      setTimeout(sendImage, 30);
     }
-  });
-} catch (e){
-  console.log("Couldn't start camera", e)
-}
+
+    connection.on('close', function(connection) {
+        // close user connection
+        clients.splice(index, 1);
+    });
+});
+console.log('Server running at http://127.0.0.1:1337/');
+
+
+
